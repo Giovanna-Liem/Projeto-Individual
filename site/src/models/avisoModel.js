aa28a0633c73c915d69d4ff8225ed2128d30ea9d
Giovanna-Liem <giovanna.liem@sptech.school>
2022-06-02 15:02:10 -0300

    correção do blog + desenvolvimento do design

--- site/src/models/avisoModel.js
+++ site/src/models/avisoModel.js
@@ -43,6 +43,9 @@ function pesquisarDescricao(texto) {
 
 function listarPorUsuario(idUsuario) {
     console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarPorUsuario()");
+    
+    console.log(idUsuario);
+    
     var instrucao = `
         SELECT 
             a.id AS idAviso,
